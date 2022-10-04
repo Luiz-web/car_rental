1ad561843066dd579e7c5a95c46a5bb770350a36
Luiz-Web <luizhbr03@gmail.com>
2022-10-04 13:36:18 -0300

    AbstractRepository created

---
+++ app/Repositories/AbstractRepository.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Repositories;
+use Illuminate\Database\Eloquent\Model;
+
+abstract class AbstractRepository {
+
+    public function __construct(Model $model) {
+        $this->model = $model;
+    }
+
+    public function selectRelationalAttributes($relational_attrs) {
+        $this->model = $this->model->with($relational_attrs);
+    }
+
+    public function filter($filters) {
+        $filters = explode(';', $filters);
+        
+        foreach($filters as $key => $condition) {
+            $c = explode(':', $condition);
+            $this->model = $this->model->where($c[0], $c[1], $c[2]);
+        }
+    }
+
+    public function selectAttributes($attrs) {
+        $this->model = $this->model->selectRaw($attrs);
+    }
+
+    public function getResult() {
+        return $this->model->get();
+    }
+
+}
+
+?>
--- app/Repositories/BrandRepository.php
+++ app/Repositories/BrandRepository.php
@@ -4,34 +4,6 @@
 
     use Illuminate\Database\Eloquent\Model;
 
-    class BrandRepository {
-        
-        public function __construct(Model $model) {
-            $this->model = $model;
-        }
-    
-        public function selectRelationalAttributes($relational_attrs) {
-            $this->model = $this->model->with($relational_attrs);
-        }
-
-        public function filter($filters) {
-            $filters = explode(';', $filters);
-            
-            foreach($filters as $key => $condition) {
-                $c = explode(':', $condition);
-                $this->model = $this->model->where($c[0], $c[1], $c[2]);
-            }
-        }
-
-        public function selectAttributes($attrs) {
-            $this->model = $this->model->selectRaw($attrs);
-        }
-
-        public function getResult() {
-            return $this->model->get();
-        }
-           
-    
-    }
+    class BrandRepository extends AbstractRepository {}
 
 ?>
